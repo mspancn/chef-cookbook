f89b0221854fce90aec68f093f846abc745b0eb4
Bo Liang <bo.liang@pearson.com>
2016-11-15 21:20:57 -0500

    Initial commit

---
+++ delayed_job/recipes/monit.rb
@@ -0,0 +1,16 @@
+# setup Unicorn monit configuration per app
+node[:deploy].each do |application, deploy|
+  next unless application == "mspan_rails"
+
+  template "/etc/monit/conf.d/delayed_job_#{application}.monitrc" do
+    source "delayed_job.monit.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables(:application => application, :deploy => deploy)
+  end
+
+  execute "monit reload" do
+    action :run
+  end
+end
